7490ec704e042311fe86ae4eb6e0b3190be57884
Alexandre Trigueros <alexandre.trigueros@changing-chameleon.com>
2024-12-20 16:13:02 +0100

    Rewrite function using pattern matching

--- exercise/C#/day18/RockPaperScissorsGame/RockPaperScissorsGame.cs
+++ exercise/C#/day18/RockPaperScissorsGame/RockPaperScissorsGame.cs
@@ -18,22 +18,18 @@ namespace RockPaperScissorsGame
 
     public static class RockPaperScissors
     {
-        public static Result? Play(Choice player1, Choice player2)
+        public static Result Play(Choice player1, Choice player2)
         {
-            if (player1 == player2)
-                return new Result(Winner.Draw, "same choice");
-            else if (player1 == Choice.Rock && player2 == Choice.Scissors)
-                return new Result(Winner.Player1, "rock crushes scissors");
-            else if (player1 == Choice.Paper && player2 == Choice.Rock)
-                return new Result(Winner.Player1, "paper covers rock");
-            else if (player1 == Choice.Scissors && player2 == Choice.Paper)
-                return new Result(Winner.Player1, "scissors cuts paper");
-            else if (player2 == Choice.Rock && player1 == Choice.Scissors)
-                return new Result(Winner.Player2, "rock crushes scissors");
-            else if (player2 == Choice.Paper && player1 == Choice.Rock)
-                return new Result(Winner.Player2, "paper covers rock");
-            else
-                return new Result(Winner.Player2, "scissors cuts paper");
+            return (player1, player2) switch
+            {
+                (Choice.Rock, Choice.Scissors) => new Result(Winner.Player1, "rock crushes scissors"),
+                (Choice.Paper, Choice.Rock) => new Result(Winner.Player1, "paper covers rock"),
+                (Choice.Scissors, Choice.Paper) => new Result(Winner.Player1, "scissors cuts paper"),
+                (Choice.Scissors, Choice.Rock) => new Result(Winner.Player2, "rock crushes scissors"),
+                (Choice.Rock, Choice.Paper) => new Result(Winner.Player2, "paper covers rock"),
+                (Choice.Paper, Choice.Scissors) => new Result(Winner.Player2, "scissors cuts paper"),
+                _ => new Result(Winner.Draw, "same choice")
+            };
         }
     }
 }
