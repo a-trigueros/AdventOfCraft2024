dfdfdab9a51f28301a40f40df680512d8dc8ff11
Alexandre Trigueros <alexandre.trigueros@changing-chameleon.com>
2024-12-20 16:15:20 +0100

    Export constants

--- exercise/C#/day18/RockPaperScissorsGame/RockPaperScissorsGame.cs
+++ exercise/C#/day18/RockPaperScissorsGame/RockPaperScissorsGame.cs
@@ -18,18 +18,20 @@ namespace RockPaperScissorsGame
 
     public static class RockPaperScissors
     {
-        public static Result Play(Choice player1, Choice player2)
-        {
-            return (player1, player2) switch
+        const string RockCrushesScissors = "rock crushes scissors";
+        const string PaperCoversRock = "paper covers rock";
+        const string ScissorsCutsPaper = "scissors cuts paper";
+        const string Draw = "same choice";
+        public static Result Play(Choice player1, Choice player2) =>
+            (player1, player2) switch
             {
-                (Choice.Rock, Choice.Scissors) => new Result(Winner.Player1, "rock crushes scissors"),
-                (Choice.Paper, Choice.Rock) => new Result(Winner.Player1, "paper covers rock"),
-                (Choice.Scissors, Choice.Paper) => new Result(Winner.Player1, "scissors cuts paper"),
-                (Choice.Scissors, Choice.Rock) => new Result(Winner.Player2, "rock crushes scissors"),
-                (Choice.Rock, Choice.Paper) => new Result(Winner.Player2, "paper covers rock"),
-                (Choice.Paper, Choice.Scissors) => new Result(Winner.Player2, "scissors cuts paper"),
-                _ => new Result(Winner.Draw, "same choice")
+                (Choice.Rock, Choice.Scissors) => new Result(Winner.Player1, RockCrushesScissors),
+                (Choice.Paper, Choice.Rock) => new Result(Winner.Player1, PaperCoversRock),
+                (Choice.Scissors, Choice.Paper) => new Result(Winner.Player1, ScissorsCutsPaper),
+                (Choice.Scissors, Choice.Rock) => new Result(Winner.Player2, RockCrushesScissors),
+                (Choice.Rock, Choice.Paper) => new Result(Winner.Player2, PaperCoversRock),
+                (Choice.Paper, Choice.Scissors) => new Result(Winner.Player2, ScissorsCutsPaper),
+                _ => new Result(Winner.Draw, Draw)
             };
-        }
     }
 }
